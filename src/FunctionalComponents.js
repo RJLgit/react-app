13039ee382762347f4ec171f181fdcc71c244e1d
Rich <r.j.lewindon@hotmail.co.uk>
2020-08-12 10:46:37 +0100

    Created framework to show more in component

--- src/FunctionalComponents.js
+++ src/FunctionalComponents.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 class FunctionalComponents extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            showMore: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+
+    handleChange() {
+
+    }
+
+
+
     render() {
        return (
            <div>
                <h1>About Functional Components</h1>
-            <button>Show more</button> 
+            <button onClick={this.handleChange}>Show more</button> 
            </div>
        )
 }
